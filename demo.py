13ae467f660dbfce028af4e81662b561c355dde4
Grandhi Akshay <139677125+akshaygrandhi123@users.noreply.github.com>
2025-06-28 09:43:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
 # This is a demo file
-print("HEllo World")
+print("HEllo World !")
+
+
+def area_of_circle(r):
+  pi = 3.14
+  return pi*r*r
+
+# Area of Circle R = 5
+r = 5
+print(area_of_circle(r))
